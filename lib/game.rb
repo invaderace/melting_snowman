c1eac80a24f7f7d627551c609078f78e939ca6ff
Michael Mattson <michael.p.mattson@gmail.com>
2020-12-29 11:35:55 -0700

    added player guess_entry and guess_valid?

--- lib/game.rb
+++ lib/game.rb
@@ -1,14 +1,38 @@
 require_relative 'dictionary'
 
 class Game
-  attr_accessor :turns_left, :word_display
+  attr_accessor :guess, :turns_left, :word_display
   attr_reader :word, :word_array
 
   def initialize
     @word = Dictionary.new.rand_word
     @turns_left = 10
+    @guess = ''
     @word_array = @word.split('')
-    @word_display = word_display
+    @word_display = word_display_setup
+  end
+  
+  def alphabet
+    ('a'..'z').to_a
+  end
+
+  def guess_enter
+    # guess is gets.chomp, so ask for the entry
+    puts guess_prompt
+    @guess = gets.chomp.downcase
+    # need to check if valid, if not, ask again. 
+    unless guess_valid?
+      puts "Invalid guess. Please try again."
+      guess_enter
+    end
+  end
+
+  def guess_prompt
+    'Please guess a letter from a-z.'
+  end
+
+  def guess_valid?
+    true if alphabet.include? guess
   end
 
   def lose?
@@ -24,9 +48,6 @@ class Game
     #if the guess is in the word, show where.
     #if the guess isn't in the word, take_turn
     # tell them the game is over if lose? is true
-  end
-
-  def word_display
   end
 
   def word_display_setup
